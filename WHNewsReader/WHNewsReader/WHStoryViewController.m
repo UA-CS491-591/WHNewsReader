9bfa027552645fa286ca04e18b6f4e10df0bb411
Caroline Godwin <clgodwin1@crimson.ua.edu>
2014-05-16 11:17:42 -0500

    merging files, but leaving old file commented out for later reference! - caroline

--- WHNewsReader/WHNewsReader/WHStoryViewController.m
+++ WHNewsReader/WHNewsReader/WHStoryViewController.m
@@ -1,6 +1,92 @@
 //
 //  WHStoryViewController.m
 //  WHNewsReader
+////
+////  Created by Caroline Godwin on 5/13/14.
+////
+////
+//
+//#import "WHStoryViewController.h"
+//#import "WHAuthorObject.h"
+//
+//@interface WHStoryViewController ()
+//@property (weak, nonatomic) IBOutlet UILabel *storyBody;
+//@property (weak, nonatomic) IBOutlet UIImageView *storyImage;
+//@property (weak, nonatomic) IBOutlet UILabel *storyTitle;
+//@property (weak, nonatomic) IBOutlet UILabel *storySubtitle;
+//@property (weak, nonatomic) IBOutlet UIView *authorMiniView;
+//@property (weak, nonatomic) IBOutlet UIImageView *authorMiniImage;
+//@property (weak, nonatomic) IBOutlet UILabel *authorName;
+//@property (weak, nonatomic) IBOutlet UILabel *authorPosition;
+//@property (weak, nonatomic) IBOutlet UIButton *authorInfo;
+//@property (weak, nonatomic) IBOutlet UILabel *storyDate;
+//@property (weak, nonatomic) IBOutlet UIImageView *navImage;
+//@property (weak, nonatomic) IBOutlet UIButton *navImageButton;
+//
+//
+//@end
+//
+//@implementation WHStoryViewController
+//
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+//
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//    // Do any additional setup after loading the view from its nib.
+//    WHStoryObject *story = _selectedStory;
+//    _storyTitle.text = story.title;
+//    _storySubtitle.text = story.subtitle;
+//    _storyBody.text = story.body;
+//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+//    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+//    [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+//    _storyDate.text = [dateFormatter stringFromDate:story.datePublished];
+//    _storyImage.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:story.imageUrl]]];
+//    
+//    [_navImageButton addTarget:self action:@selector(didTapMyButton:) forControlEvents:UIControlEventTouchUpInside];
+//    [self.view addSubview:_navImage];
+//    [self.view addSubview:_navImageButton];
+//    
+//
+//
+//
+//    
+//    WHAuthorObject *author = [[WHAuthorObject alloc] init];
+//     _authorMiniImage.image = [NSURL URLWithString:author.imageUrl];
+//      _authorName.text = [NSString stringWithFormat:@"%@ %@", author.firstName, author.lastName]  ;
+//    _authorPosition.text = author.position;
+//    
+//}
+//
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//
+//-(void)didTapMyButton:(UIButton *)sender{
+////    [sender setTitle:@"Did Tap Button" forState:UIControlStateNormal];
+////    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://www.github.com/carolinegodwin"]];
+//
+////Create alert
+//       UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Story Location" message:@"My Message" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+//    //    //Show Alert
+//        [alert show];
+//    
+//}
+//
+//@end
+//
+//  WHStoryViewController.m
+//  WHNewsReader
 //
 //  Created by Caroline Godwin on 5/13/14.
 //
@@ -20,8 +106,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *authorPosition;
 @property (weak, nonatomic) IBOutlet UIButton *authorInfo;
 @property (weak, nonatomic) IBOutlet UILabel *storyDate;
-@property (weak, nonatomic) IBOutlet UIImageView *navImage;
-@property (weak, nonatomic) IBOutlet UIButton *navImageButton;
 
 
 @end
@@ -51,17 +135,9 @@
     _storyDate.text = [dateFormatter stringFromDate:story.datePublished];
     _storyImage.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:story.imageUrl]]];
     
-    [_navImageButton addTarget:self action:@selector(didTapMyButton:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:_navImage];
-    [self.view addSubview:_navImageButton];
-    
-
-
-
-    
     WHAuthorObject *author = [[WHAuthorObject alloc] init];
-     _authorMiniImage.image = [NSURL URLWithString:author.imageUrl];
-      _authorName.text = [NSString stringWithFormat:@"%@ %@", author.firstName, author.lastName]  ;
+    _authorMiniImage.image = [NSURL URLWithString:author.imageUrl];
+    _authorName.text = [NSString stringWithFormat:@"%@ %@", author.firstName, author.lastName]  ;
     _authorPosition.text = author.position;
     
 }
@@ -70,17 +146,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
--(void)didTapMyButton:(UIButton *)sender{
-//    [sender setTitle:@"Did Tap Button" forState:UIControlStateNormal];
-//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://www.github.com/carolinegodwin"]];
-
-//Create alert
-       UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Story Location" message:@"My Message" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
-    //    //Show Alert
-        [alert show];
-    
 }
 
 @end
