d63e7dbd545e46c45e7774d2983675baf236a1ed
rthill <rthill@crimson.ua.edu>
2014-05-16 10:10:26 -0500

    Fixed alignment of recent stories tableview

--- WHNewsReader/WHNewsReader/WHRecentTableViewController.m
+++ WHNewsReader/WHNewsReader/WHRecentTableViewController.m
@@ -9,6 +9,7 @@
 #import "WHRecentTableViewController.h"
 #import "WHRecentObject.h"
 #import "WHRecentTableViewCell.h"
+#import "WHDataRetrieval.h"
 
 @interface WHRecentTableViewController ()
 
@@ -23,27 +24,22 @@
 {
     WHRecentObject *item1 = [[WHRecentObject alloc] init];
     item1.name = @"Story 1";
-    [self.recentItems addObject:item1];
+    [_recentItems addObject:item1];
     WHRecentObject *item2 = [[WHRecentObject alloc] init];
     item2.name = @"Story 2";
-    [self.recentItems addObject:item2];
+    [_recentItems addObject:item2];
     WHRecentObject *item3 = [[WHRecentObject alloc] init];
     item3.name = @"Story 3";
-    [self.recentItems addObject:item3];
+    [_recentItems addObject:item3];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.recentItems = [[NSMutableArray alloc] init];
-    [self loadTestData];
-    //[self sendAsynchronousRequest];
+    _recentItems = [[NSMutableArray alloc] init];
+    //[self loadTestData];
+    [self makeRequest];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -82,35 +78,33 @@
     return cell;
 }
 
-#pragma mark - Story list connection
-
--(void)sendAsynchronousRequest{
-    NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+-(void)makeRequest
+{
+    NSString *userToken = @"b7a2ac80-67a7-41bb-a7ff-8e6574b0bdf2";;
     
-    //Create url
-    NSString *userToken = @"b7a2ac80-67a7-41bb-a7ff-8e6574b0bdf2";
-    NSString *stringURL = [NSString stringWithFormat:@"https://mobileweb.caps.ua.edu/cs491/api/Story/recent?token=%@",userToken];
-    NSURL *url = [NSURL URLWithString:stringURL];
-    
-    //Create request object
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
-    
-    //Make request
-    [NSURLConnection sendAsynchronousRequest:request queue:operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        
-        //3. Handle response
+    [WHDataRetrieval getStoryRecent:userToken completetionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
         NSString *responseString = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+        //NSLog(@"%@",responseString);
         
-        //Hop back to main thread to update UI
-        dispatch_async(dispatch_get_main_queue(), ^{
-            //_responseTextView.text = responseString;
-            NSLog(@"%@",responseString);
-        });
+        NSArray *array = [responseString componentsSeparatedByString:@","];
+        //NSLog(@"%@",test);
+        
+        for(NSString *title in array)
+        {
+            if([title rangeOfString:@"\"title\":"].location != NSNotFound)
+            {
+                WHRecentObject *item = [[WHRecentObject alloc] init];
+                item.name = title;
+                [_recentItems addObject:item];
+                //NSLog(@"%@",title);
+                NSLog(@"%@",self.recentItems);
+                
+            }
+        }
         
     }];
+    //NSLog(@"%@",self.recentItems);
 }
-
-
 
 
 
